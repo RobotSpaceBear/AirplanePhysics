aed28152be8943fb48acee818e4f9c7ed9f31d4a
RobotSpaceBear <47452069+RobotSpaceBear@users.noreply.github.com>
2020-05-11 23:39:12 +0200

    added blurred propeller textures and texture swap logic for RPM

--- Assets/AirplanePhysics/Code/Scripts/Propellers/IP_Airplane_Propeller.cs
+++ Assets/AirplanePhysics/Code/Scripts/Propellers/IP_Airplane_Propeller.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace IndiePixel
@@ -7,12 +8,67 @@ namespace IndiePixel
     public class IP_Airplane_Propeller : MonoBehaviour
     {
 
+        [Header("Propeller Properties")]
+        public float minQuadRPM = 300f;
+        public float minTextureSwap = 600;
+        public GameObject mainProp;
+        public GameObject blurredProp;
 
+        [Header("Material Properties")]
+        public Material blurredPropMat;
+        public Texture2D blurLevel1;
+        public Texture2D blurLevel2;
+
+
+        private void Start()
+        {
+            if (mainProp && blurredProp)
+            {
+                HandleSwapping(0f);
+            }
+        }
         public void HandlePropeller(float currentRPM)
         {
             // dps = degrees per second
-            float dps = ((currentRPM * 360) / 60 )* Time.deltaTime;
+            float dps = ((currentRPM * 360) / 60) * Time.deltaTime;
             transform.Rotate(Vector3.forward, dps);
+
+            if (mainProp && blurredProp)
+            {
+                HandleSwapping(currentRPM);
+            }
         }
-    } 
+
+        /// <summary>
+        /// Swapping materials and textures of the prop depending on the RPM
+        /// </summary>
+        /// <param name="currentRPM"></param>
+        void HandleSwapping(float currentRPM)
+        {
+            if (currentRPM > minQuadRPM)
+            {
+                blurredProp.gameObject.SetActive(true);
+                mainProp.gameObject.SetActive(false);
+
+                if (blurredPropMat && blurLevel1 && blurLevel2)
+                {
+                    if (currentRPM > minTextureSwap)
+                    {
+                        /* got "_MainTex" by inspecting the properties of the MAT used for the blur prop Quad. 
+                         * Click the cog on top right of material and see shader. Texture is on Albedo channel.*/
+                        blurredPropMat.SetTexture("_MainTex", blurLevel2);
+                    }
+                    else
+                    {
+                        blurredPropMat.SetTexture("_MainTex", blurLevel1);
+                    }
+                }
+            }
+            else
+            {
+                blurredProp.gameObject.SetActive(false);
+                mainProp.gameObject.SetActive(true);
+            }
+        }
+    }
 }
